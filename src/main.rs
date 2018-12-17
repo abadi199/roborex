a5f71f3e58b9a272c0c56d0bcbc58fedbc7cd8e6
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2018-12-18 04:56:52 +0700

    Add splash screen.

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ mod player;
 mod player_state;
 mod primitive;
 mod puzzle;
+mod splash;
 
 use level::Level;
 use player::Player;
@@ -30,11 +31,19 @@ use quicksilver::{
     lifecycle::{run, Settings, State, Window},
     Result,
 };
+use splash::Splash;
 
 struct RoboRex {
     time: f64,
     player: Player,
     level: Level,
+    state: GameState,
+    splash: Splash,
+}
+
+enum GameState {
+    Splash,
+    Playing,
 }
 
 impl RoboRex {
@@ -42,23 +51,8 @@ impl RoboRex {
         self.level = level;
         self.player.position = self.level.start_position.clone();
     }
-}
 
-impl State for RoboRex {
-    fn new() -> Result<RoboRex> {
-        let level = Level::start();
-        let mut player = Player::new();
-        player.position = level.start_position.clone();
-        let roborex = RoboRex {
-            time: 0.,
-            player,
-            level,
-        };
-
-        Ok(roborex)
-    }
-
-    fn update(&mut self, window: &mut Window) -> Result<()> {
+    fn update_playing(&mut self, window: &mut Window) -> Result<()> {
         self.time += window.update_rate();
         self.level.update(window, &mut self.player)?;
 
@@ -73,10 +67,56 @@ impl State for RoboRex {
         Ok(())
     }
 
-    fn draw(&mut self, window: &mut Window) -> Result<()> {
+    fn update_splash(&mut self, window: &mut Window) -> Result<()> {
+        self.splash.update(window)?;
+        if (self.splash.is_clicked()) {
+            self.state = GameState::Playing;
+        }
+
+        Ok(())
+    }
+
+    fn draw_playing(&mut self, window: &mut Window) -> Result<()> {
         window.clear(Color::BLACK)?;
         self.level.draw(window, &mut self.player)?;
         Ok(())
+    }
+
+    fn draw_splash(&mut self, window: &mut Window) -> Result<()> {
+        self.splash.draw(window)?;
+        Ok(())
+    }
+}
+
+impl State for RoboRex {
+    fn new() -> Result<RoboRex> {
+        let splash = Splash::new();
+        let level = Level::start();
+        let mut player = Player::new();
+        player.position = level.start_position.clone();
+        let roborex = RoboRex {
+            time: 0.,
+            player,
+            level,
+            splash,
+            state: GameState::Splash,
+        };
+
+        Ok(roborex)
+    }
+
+    fn update(&mut self, window: &mut Window) -> Result<()> {
+        match self.state {
+            GameState::Splash => self.update_splash(window),
+            GameState::Playing => self.update_playing(window),
+        }
+    }
+
+    fn draw(&mut self, window: &mut Window) -> Result<()> {
+        match self.state {
+            GameState::Splash => self.draw_splash(window),
+            GameState::Playing => self.draw_playing(window),
+        }
     }
 }
 
---
+++ src/splash.rs
@@ -0,0 +1,48 @@
+use quicksilver::{
+    graphics::{Background::Img, Image},
+    input::{ButtonState, MouseButton},
+    lifecycle::{Asset, Window},
+    Result,
+};
+
+pub struct Splash {
+    state: State,
+    image: Asset<Image>,
+}
+
+enum State {
+    Waiting,
+    Clicked,
+}
+
+impl Splash {
+    pub fn new() -> Self {
+        let image = Asset::new(Image::load("resources/images/splash.png"));
+        let state = State::Waiting;
+
+        Self { state, image }
+    }
+
+    pub fn is_clicked(&self) -> bool {
+        match self.state {
+            State::Waiting => false,
+            State::Clicked => true,
+        }
+    }
+
+    pub fn update(&mut self, window: &mut Window) -> Result<()> {
+        if window.mouse()[MouseButton::Left] == ButtonState::Released {
+            self.state = State::Clicked;
+        }
+
+        Ok(())
+    }
+
+    pub fn draw(&mut self, window: &mut Window) -> Result<()> {
+        self.image.execute(|image| {
+            window.draw(&image.area(), Img(&image));
+            Ok(())
+        })?;
+        Ok(())
+    }
+}
